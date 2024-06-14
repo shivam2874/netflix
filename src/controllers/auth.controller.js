8b18586e27539c3f130cf4564aa99180915edd81
Shivam <techbulls2021@gmail.com>
2024-06-14 15:19:06 +0530

    Generated Acess Token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,23 +1,33 @@
 import { createUser } from "../services/auth.service.js";
+import { generateToken } from "../services/token.service.js";
 
 export const register = async (req, res, next) => {
-  const { name, email, password, picture } = req.body;
-
-  const user = await createUser({
-    name,
-    email,
-    picture,
-    password,
-  });
-
   try {
-    res.send(user);
+    const { name, email, password, picture } = req.body;
+
+    const user = await createUser({
+      name,
+      email,
+      picture,
+      password,
+    });
+    // Generate the access token
+    const access_token = await generateToken(
+      { userId: user._id },
+      process.env.TOKEN_EXPIRE,
+      process.env.ACCESS_TOKEN_SECRET
+    );
+
+    res.status(200).json({
+      user,
+      access_token,
+    });
   } catch (error) {
     next(error);
   }
 };
 export const login = async (req, res, next) => {
-  res.send("Hello from Login");
+  const { email, password } = req.body;
 };
 export const logout = async (req, res, next) => {
   res.send("Hello from Logout");
