2451575d2f878a6525980d3468d4ffadc92867a1
Shivam <techbulls2021@gmail.com>
2024-06-18 12:23:50 +0530

    Added all CRUD operation controllers

--- src/controllers/movie.controller.js
+++ src/controllers/movie.controller.js
@@ -1,7 +1,30 @@
 import Movie from "../models/movieModel.js";
 
+/**Creating Movie */
+export const createMovie = async (req, res, next) => {
+  try {
+    const newMovie = await Movie.create(req.body);
+
+    res.status(201).json({
+      status: "Sucess",
+      movie: {
+        newMovie,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**Reading All Movies */
 export const allMovies = async (req, res, next) => {
-  const movies = await Movie.find();
+  const queryObj = { ...req.query };
+  const excludedFields = ["page", "sort", "limit", "fields"];
+
+  excludedFields.forEach((el) => delete queryObj[el]);
+
+  const movies = await Movie.find(queryObj);
+
   try {
     res.status(200).json({
       results: movies.length,
@@ -14,13 +37,11 @@ export const allMovies = async (req, res, next) => {
   }
 };
 
+/**Reading  Movie by ID */
 export const getMovie = async (req, res, next) => {
   const id = req.params.id;
+  const movie = await Movie.findById(id);
   try {
-    const movie = await Movie.findById(id);
-    if (!movie) {
-      res.send("Hello");
-    }
     res.status(200).json({
       status: "success",
       data: {
@@ -28,7 +49,7 @@ export const getMovie = async (req, res, next) => {
       },
     });
   } catch (error) {
-    if (error.name === "CastError") {
+    if (error.name === "CastError" || movie == null) {
       res.status(404).json({
         status: "Failed",
         message: "Invalid Id",
@@ -37,3 +58,53 @@ export const getMovie = async (req, res, next) => {
     next(error);
   }
 };
+
+/**Updating Movie */
+export const updateMovie = async (req, res, next) => {
+  try {
+    const updatedMovie = await Movie.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    res.status(200).json({
+      status: "sucess",
+      movie: {
+        updatedMovie,
+      },
+    });
+  } catch (error) {
+    if (error.anme === "CastError") {
+      res.status(404).json({
+        status: "Failed",
+        message: "Invalid Id",
+      });
+    }
+    next(error);
+  }
+};
+
+/**Deleting Movie */
+export const deleteMovie = async (req, res, next) => {
+  try {
+    const movie = await Movie.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: "Sucess",
+      data: null,
+    });
+  } catch (error) {
+    if (error.anme === "CastError") {
+      res.status(404).json({
+        status: "Failed",
+        message: "Invalid Id",
+      });
+    }
+    next(error);
+  }
+};
+9;
--- src/routes/movie.route.js
+++ src/routes/movie.route.js
@@ -1,8 +1,21 @@
 import express from "express";
-import { allMovies, getMovie } from "../controllers/movie.controller.js";
+import {
+  allMovies,
+  getMovie,
+  updateMovie,
+  deleteMovie,
+  createMovie,
+} from "../controllers/movie.controller.js";
+import trimRequest from "trim-request";
 
 const router = express.Router();
 
-router.route("/movies/:id").get(getMovie);
-router.route("/movies").get(allMovies);
+router
+  .route("/movies/:id")
+  .get(getMovie)
+  .delete(deleteMovie)
+  .post(trimRequest.all, updateMovie);
+
+router.route("/movies").get(allMovies).post(trimRequest.all, createMovie);
+
 export default router;
